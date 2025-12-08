31fb1db90e9394dfbc689e655698813453724d72
Mohamed Imthiyas <115208546+Imthiyass@users.noreply.github.com>
2025-12-09 03:02:32 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-function App() {
+export default function App() {
   return (
     <div className="p-10 text-center">
-      <h1 className="text-4xl font-bold text-white">Welcome to My Portfolio</h1>
+      <h1 className="text-4xl font-bold text-white">
+        Welcome to My Portfolio
+      </h1>
     </div>
   );
 }
-
-export default App;
