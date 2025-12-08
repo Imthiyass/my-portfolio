ce8757b876f7f9168f5f9eff9a528931d8ea07c6
Mohamed Imthiyas <115208546+Imthiyass@users.noreply.github.com>
2025-12-09 02:57:58 +0800

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="p-10 text-center">
+      <h1 className="text-4xl font-bold text-white">Welcome to My Portfolio</h1>
+    </div>
+  );
+}
+
+export default App;
